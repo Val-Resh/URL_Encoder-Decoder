b5ce32af801c80193ae0395df5b532da2dc345e7
Val-Resh <80176765+Val-Resh@users.noreply.github.com>
2021-11-12 17:43:04 +0000

    Add files via upload

---
+++ urlcoder.py
@@ -0,0 +1,18 @@
+# simple script meant to allow to encode and decode URLs.
+
+import urllib.parse as parser
+import sys
+
+def main(argv):
+      if argv[0] == '-h':
+          print("Encode: urlcoder -e <string_to_encode>")
+          print("Decode: urlcoder -d <string_to_decode>")
+      elif argv[0] == '-e':
+          print("\n" + parser.quote(" ".join(argv[1:])))
+      elif argv[0] == '-d':
+          print("\n" + parser.unquote("\n" + " ".join(argv[1:])))
+      else:
+          print("Invalid input. Use -h for help.")
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
